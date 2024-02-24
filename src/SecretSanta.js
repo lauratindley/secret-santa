a2e9974081a5a7a2f803791d8f0282afdedf7df1
lauratindley <142441126+lauratindley@users.noreply.github.com>
2024-02-24 13:47:31 +0000

    added bootstrap & button

--- src/SecretSanta.js
+++ src/SecretSanta.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./SecretSanta.css";
 
 export default function SecretSanta() {
@@ -9,15 +10,34 @@ export default function SecretSanta() {
       <form>
         <ul className="names">
           <li>
-            <input type="checkbox" id="lauraTindley" className="name"></input>
+            <input type="checkbox" id="lauraT" className="name"></input>
             <label className="name">Laura Tindley</label>
           </li>
           <li>
-            <input type="checkbox" id="lucyScaramanga" className="name"></input>
+            <input type="checkbox" id="lucyS" className="name"></input>
             <label className="name">Lucy Scaramanga</label>
+          </li>
+          <li>
+            <input type="checkbox" id="sarahPJ" className="name"></input>
+            <label className="name">Sarah Parry-Jones</label>
+          </li>
+          <li>
+            <input type="checkbox" id="katieW" className="name"></input>
+            <label className="name">Katie Worthington</label>
+          </li>
+          <li>
+            <input type="checkbox" id="samD" className="name"></input>
+            <label className="name">Sam Dickinson</label>
+          </li>
+          <li>
+            <input type="checkbox" id="SteveL" className="name"></input>
+            <label className="name">Steve Long</label>
           </li>
         </ul>
       </form>
+      <button type="button" class="btn btn-danger m-4">
+        Create pairs
+      </button>
     </div>
   );
 }
